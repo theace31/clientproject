2f7af0f3d5a2e9035f9944f51c3a2d9adbfd1ca8
theace31 <bluecreative31@gmail.com>
2016-11-19 08:18:00 -0800

    javascript update

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by hansguignard on 11/19/16.
+ */
+/**
+ * Created by hansguignard on 11/18/16.
+ */
+var price;
+var quantity;
+var total;
+
+
+
+price = 5;
+quantity =14;
+total = price * quantity;
+
+var el =document.getElementbyId('cost');
+el.textContent ='$' + total;
